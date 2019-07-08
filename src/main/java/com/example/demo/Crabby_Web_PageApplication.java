bb04e3989ad6571d511667ac905d9acd65ab6d2e
kellel14 <kellel14@gmail.com>
2019-07-08 13:36:58 -0400

    first commit: basic webpage using thymeleaf

---
+++ src/main/java/com/example/demo/Crabby_Web_PageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crabby_Web_PageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crabby_Web_PageApplication.class, args);
+	}
+
+}
